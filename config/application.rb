19249fc1b817e8c42fe7839449e4d4fe7d82699d
Ariel Diaz Bermejo <arieldiazbermejo@gmail.com>
2012-10-24 18:13:37 -0300

    Fixing assets precompile for Heroku

--- config/application.rb
+++ config/application.rb
@@ -76,6 +76,9 @@ module MyTodo
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # == To deploy on Heroku
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
